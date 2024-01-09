c4224d0d9ab88cc337ee9a023913dfd0836cf140
Valerio Bottinelli <valerio.bottinelli@hftm.ch>
2024-01-09 17:59:56 +0100

    some kind of logging included.. no idea how this works though...

--- lolorbwars/src/main/java/com/lolorbwars/Battlefield.java
+++ lolorbwars/src/main/java/com/lolorbwars/Battlefield.java
@@ -20,6 +20,9 @@ import javafx.fxml.FXML;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.util.logging.LogManager;
 
 public class Battlefield {
 
@@ -31,16 +34,20 @@ public class Battlefield {
   static Player p2;
 
   Draw draw;
+  // Logger initialize
+  public static final Logger LOG = System.getLogger(Battlefield.class.getSimpleName());
 
   @FXML
   public void initialize() {
+    // here i want to output something with my logger....
     p1 = new Player(1, Color.RED, 50, 600);
     p2 = new Player(2, Color.BLUE, 750, 600);
     draw = new Draw(p1, p2);
     battlefieldNode.requestFocus();
     keysEvents = new KeyEvents(draw, battlefieldNode, p1, p2);
     if (true == MenuControll.isNewGame()) {
-      System.out.println("New Game has started");
+      LOG.log(Level.INFO, "new game has started");
+      // System.out.println("New Game has started");
     } else {
       System.out.println("The game continues");
       System.out.println("Battlefield Initialization successfull");
--- lolorbwars/src/main/java/module-info.java
+++ lolorbwars/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.lolorbwars {
     requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
 
     opens com.lolorbwars to javafx.fxml;
+
     exports com.lolorbwars;
 }
